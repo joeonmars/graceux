4cbcac424c143df019f14c09c65fd3a989aff7d1
joeonmars <joe@joeonmars.com>
2015-08-26 23:52:27 -0400

    updates;

--- source/assets/js/project/controllers/modules/workflow.js
+++ source/assets/js/project/controllers/modules/workflow.js
@@ -1,22 +1,22 @@
-goog.provide('gux.controllers.modules.Workflow');
+goog.provide( 'gux.controllers.modules.Workflow' );
 
-goog.require('goog.fx.Dragger');
-goog.require('gux.controllers.Module');
+goog.require( 'goog.fx.Dragger' );
+goog.require( 'gux.controllers.Module' );
 
 
-gux.controllers.modules.Workflow = function(element) {
+gux.controllers.modules.Workflow = function( element ) {
 
-	goog.base(this, element);
+	goog.base( this, element );
 
-	this._imageContainer = goog.dom.query('.image-container', this.el)[0];
-	this._scroller = goog.dom.query('.scroller', this.el)[0];
-	this._prevButton = goog.dom.query('.prev', this.el)[0];
-	this._nextButton = goog.dom.query('.next', this.el)[0];
-	this._stepButtons = goog.dom.query('.pagination button', this.el);
+	this._imageContainer = goog.dom.query( '.image-container', this.el )[ 0 ];
+	this._scroller = goog.dom.query( '.scroller', this.el )[ 0 ];
+	this._prevButton = goog.dom.query( '.prev', this.el )[ 0 ];
+	this._nextButton = goog.dom.query( '.next', this.el )[ 0 ];
+	this._stepButtons = goog.dom.query( '.pagination button', this.el );
 
-	this._dragger = new goog.fx.Dragger(this._scroller);
-	this._dragger.setHysteresis(10);
-	this._dragger.defaultAction = goog.bind(this.onDrag, this);
+	this._dragger = new goog.fx.Dragger( this._scroller );
+	this._dragger.setHysteresis( 10 );
+	this._dragger.defaultAction = goog.bind( this.onDrag, this );
 
 	this._scrollProps = {
 		x1: 0,
@@ -30,40 +30,40 @@ gux.controllers.modules.Workflow = function(element) {
 	};
 
 	this._step = 0;
-	this._numSteps = goog.dom.query('li', this._scroller).length;
+	this._numSteps = goog.dom.query( 'li', this._scroller ).length;
 
 	this.activate();
 };
-goog.inherits(gux.controllers.modules.Workflow, gux.controllers.Module);
+goog.inherits( gux.controllers.modules.Workflow, gux.controllers.Module );
 
 
 gux.controllers.modules.Workflow.prototype.disposeInternal = function() {
 
-	goog.base(this, 'disposeInternal');
+	goog.base( this, 'disposeInternal' );
 };
 
 
 gux.controllers.modules.Workflow.prototype.doActivate = function() {
 
-	goog.base(this, 'doActivate');
+	goog.base( this, 'doActivate' );
 
-	this._eventHandler.listen(this._dragger, goog.fx.Dragger.EventType.START, this.onDragStart, false, this);
-	this._eventHandler.listen(this._dragger, goog.fx.Dragger.EventType.END, this.onDragEnd, false, this);
+	this._eventHandler.listen( this._dragger, goog.fx.Dragger.EventType.START, this.onDragStart, false, this );
+	this._eventHandler.listen( this._dragger, goog.fx.Dragger.EventType.END, this.onDragEnd, false, this );
 
-	this._eventHandler.listen(this._prevButton, goog.events.EventType.CLICK, this.prevStep, false, this);
-	this._eventHandler.listen(this._nextButton, goog.events.EventType.CLICK, this.nextStep, false, this);
+	this._eventHandler.listen( this._prevButton, goog.events.EventType.CLICK, this.prevStep, false, this );
+	this._eventHandler.listen( this._nextButton, goog.events.EventType.CLICK, this.nextStep, false, this );
 
-	this._eventHandler.listen(window, goog.events.EventType.RESIZE, this.resize, false, this);
+	this._eventHandler.listen( window, goog.events.EventType.RESIZE, this.resize, false, this );
 
-	goog.array.forEach(this._stepButtons, function(el) {
-		this._eventHandler.listen(el, goog.events.EventType.CLICK, this.onClickStepButton, false, this);
-	}, this);
+	goog.array.forEach( this._stepButtons, function( el ) {
+		this._eventHandler.listen( el, goog.events.EventType.CLICK, this.onClickStepButton, false, this );
+	}, this );
 };
 
 
 gux.controllers.modules.Workflow.prototype.doDeactivate = function() {
 
-	goog.base(this, 'doDeactivate');
+	goog.base( this, 'doDeactivate' );
 
 	this._dragger.dispose();
 };
@@ -71,50 +71,48 @@ gux.controllers.modules.Workflow.prototype.doDeactivate = function() {
 
 gux.controllers.modules.Workflow.prototype.prevStep = function() {
 
-	var step = Math.max(0, this._step - 1);
-	this.scrollToStep(step);
+	var step = Math.max( 0, this._step - 1 );
+	this.scrollToStep( step );
 };
 
 
 gux.controllers.modules.Workflow.prototype.nextStep = function() {
 
-	var step = Math.min(this._numSteps - 1, this._step + 1);
-	this.scrollToStep(step);
+	var step = Math.min( this._numSteps - 1, this._step + 1 );
+	this.scrollToStep( step );
 };
 
 
-gux.controllers.modules.Workflow.prototype.scrollToStep = function(step) {
+gux.controllers.modules.Workflow.prototype.scrollToStep = function( step ) {
 
 	this._step = step;
 
-	var stepWidth = goog.style.getSize(this._imageContainer).width;
+	var stepWidth = goog.style.getSize( this._imageContainer ).width;
 	var stepX = stepWidth * this._step;
 
-	var duration = goog.math.lerp(.25, .65, Math.abs(stepX - this._scroller.scrollLeft) / stepWidth);
-
-	var imageTweener = TweenMax.to(this._scroller, duration, {
+	var imageTweener = TweenMax.to( this._scroller, .6, {
 		'scrollTo': {
 			'x': stepX
 		},
-		'ease': Cubic.easeOut
-	});
+		'ease': Quad.easeOut
+	} );
 
-	goog.array.forEach(this._stepButtons, function(el, index) {
-		goog.dom.classlist.enable(el, 'active', (step === index));
-	});
+	goog.array.forEach( this._stepButtons, function( el, index ) {
+		goog.dom.classlist.enable( el, 'active', ( step === index ) );
+	} );
 
-	this._prevButton.disabled = (step === 0);
-	this._nextButton.disabled = (step === this._numSteps - 1);
+	this._prevButton.disabled = ( step === 0 );
+	this._nextButton.disabled = ( step === this._numSteps - 1 );
 };
 
 
 gux.controllers.modules.Workflow.prototype.resize = function() {
 
-	this.scrollToStep(this._step);
+	this.scrollToStep( this._step );
 };
 
 
-gux.controllers.modules.Workflow.prototype.onDragStart = function(e) {
+gux.controllers.modules.Workflow.prototype.onDragStart = function( e ) {
 
 	var touchX = this._dragger.clientX;
 
@@ -126,28 +124,28 @@ gux.controllers.modules.Workflow.prototype.onDragStart = function(e) {
 
 	this._scrollProps.originalX = this._scroller.scrollLeft;
 
-	var deltaX = Math.abs(this._dragger.startX - this._dragger.clientX);
-	var deltaY = Math.abs(this._dragger.startY - this._dragger.clientY);
-	this._scrollProps.isVertical = (deltaX < deltaY);
+	var deltaX = Math.abs( this._dragger.startX - this._dragger.clientX );
+	var deltaY = Math.abs( this._dragger.startY - this._dragger.clientY );
+	this._scrollProps.isVertical = ( deltaX < deltaY );
 };
 
 
-gux.controllers.modules.Workflow.prototype.onDragEnd = function(e) {
+gux.controllers.modules.Workflow.prototype.onDragEnd = function( e ) {
 
-	if (this._scrollProps.isVertical) return;
+	if ( this._scrollProps.isVertical ) return;
 
-	var elapsedTime = (goog.now() - this._scrollProps.t2) / 1000;
+	var elapsedTime = ( goog.now() - this._scrollProps.t2 ) / 1000;
 	var threshold = 100;
-	var viewportMid = goog.style.getSize(this._imageContainer).width / 2;
-	var velocity = (this._scrollProps.endX - this._scrollProps.x2) / elapsedTime;
+	var viewportMid = goog.style.getSize( this._imageContainer ).width / 2;
+	var velocity = ( this._scrollProps.endX - this._scrollProps.x2 ) / elapsedTime;
 	var scrolledDistance = this._scrollProps.endX - this._scrollProps.startX;
 
-	if (velocity > threshold || scrolledDistance > viewportMid) {
+	if ( velocity > threshold || scrolledDistance > viewportMid ) {
 
 		// go prev step
 		this.prevStep();
 
-	} else if (velocity < -threshold || scrolledDistance < -viewportMid) {
+	} else if ( velocity < -threshold || scrolledDistance < -viewportMid ) {
 
 		// go next step
 		this.nextStep();
@@ -155,15 +153,15 @@ gux.controllers.modules.Workflow.prototype.onDragEnd = function(e) {
 	} else {
 
 		// return to current step
-		this.scrollToStep(this._step);
+		this.scrollToStep( this._step );
 
 	}
 };
 
 
-gux.controllers.modules.Workflow.prototype.onDrag = function(x, y) {
+gux.controllers.modules.Workflow.prototype.onDrag = function( x, y ) {
 
-	if (this._scrollProps.isVertical) return;
+	if ( this._scrollProps.isVertical ) return;
 
 	var touchX = this._dragger.clientX;
 
@@ -180,11 +178,11 @@ gux.controllers.modules.Workflow.prototype.onDrag = function(x, y) {
 };
 
 
-gux.controllers.modules.Workflow.prototype.onClickStepButton = function(e) {
+gux.controllers.modules.Workflow.prototype.onClickStepButton = function( e ) {
 
-	var step = parseInt(e.currentTarget.getAttribute('data-step'));
+	var step = parseInt( e.currentTarget.getAttribute( 'data-step' ) );
 
-	if (this._step !== step) {
-		this.scrollToStep(step);
+	if ( this._step !== step ) {
+		this.scrollToStep( step );
 	}
 };
